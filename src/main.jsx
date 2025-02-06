ba8c6fa407ed4d51f7b8e67474cd98fbd3a2de0e
tarpeeton <tarpeetonteam@gmail.com>
2025-02-07 10:59:35 +1400

    http 2 protocol error

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {App} from './App'
-import { HelmetProvider } from "react-helmet-async";
+import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import './index.css';
+import { App } from './App';
 
 createRoot(document.getElementById('root')).render(
     <HelmetProvider>
-      <App />
+        <App />
     </HelmetProvider>
-)
+);
